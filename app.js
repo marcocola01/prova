6baa7fd07a14189279a3900fe172d53fb1829c48
marcocola01 <colangelo.marco@iisgalvanimi.edu.it>
2020-02-02 10:53:50 +0000

    continuazione

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('poeti', poetiRouter);
+app.use('/poeti', poetiRouter);
 
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/web/poeti:email', function(req, res, next) {
+ let poeta_trovato = file_poeti.poeti.find(poeta => poeta.email == req.params.email);
+    res.render('poeti', { title: 'Lista Poeti', poeta: poeta_trovato});
+});
+
 module.exports = router;
