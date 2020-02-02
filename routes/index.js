eb9db5eb4a8630e6d8531d5747647dee6e690157
marcocola01 <colangelo.marco@iisgalvanimi.edu.it>
2020-02-02 15:45:48 +0000

    continuazione

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var file_poeti = require('../poeti.json');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
 
 router.get('/web/poeti:email', function(req, res, next) {
  let poeta_trovato = file_poeti.poeti.find(poeta => poeta.email == req.params.email);
     res.render('poeti', { title: 'Lista Poeti', poeta: poeta_trovato});
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', poeti: file_poeti.poeti });
 });
 
 module.exports = router;
